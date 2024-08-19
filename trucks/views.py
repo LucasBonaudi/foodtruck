88aee802e870e1e0b71c123f3392ccaa0dade9fa
Lucas Bonaudi <lucasbonaudi@gmail.com>
2024-08-19 12:46:34 -0300

    New parameter to filter food trucks by food type

--- trucks/views.py
+++ trucks/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from requests import Response
 from rest_framework import generics
 from .models import FoodTruck
 from .serializers import FoodTruckSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class FoodTruckListAPI(generics.ListAPIView):
     queryset = FoodTruck.objects.all()
     serializer_class = FoodTruckSerializer
     
+    def get_queryset(self):
+        queryset = FoodTruck.objects.all()
+        fooditem = self.request.GET.get('fooditem', None)
+
+        print(fooditem)
+        
+        if fooditem:
+            queryset = queryset.filter(foodItems__icontains=fooditem)
+        
+        return queryset
